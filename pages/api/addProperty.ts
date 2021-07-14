a0b50e332fa8b1d1cf3c582275f792bebbe60a52
Grace Wang <gracewang92@gmail.com>
2021-07-14 12:01:50 -0700

    fix: rename backend parking_fee -> trash_fee

--- pages/api/addProperty.ts
+++ pages/api/addProperty.ts
@@ -6,7 +6,7 @@ async function handler(
   req: RequireSessionProp<NextApiRequest>,
   res: NextApiResponse
 ) {
-  const query = "insert into properties(user_id, address, city, state, zip, application_fee, reservation_fee, admin_fee, parking_fee, pet_fee, custom_text) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)";
+  const query = "insert into properties(user_id, address, city, state, zip, application_fee, reservation_fee, admin_fee, trash_fee, pet_fee, custom_text) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)";
   await client.query(query, [
     req.session.userId,
     req.body.address,
@@ -16,7 +16,7 @@ async function handler(
     req.body.application_fee,
     req.body.reservation_fee,
     req.body.admin_fee,
-    req.body.parking_fee,
+    req.body.trash_fee,
     req.body.pet_fee,
     req.body.custom_text,
   ]);
