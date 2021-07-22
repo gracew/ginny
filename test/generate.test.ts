100783e6e4cd291ac15863552431a8df2394832c
Jason <jrzhong2009@gmail.com>
2021-07-22 14:29:44 -0400

    add a test on the trimming of the file name

--- test/generate.test.ts
+++ test/generate.test.ts
@@ -4,6 +4,7 @@ import { docxName } from "../pages/api/generate";
 it("generated doc name", () => {
   const currentMoment = moment();
   const currentMomentFormatted = currentMoment.format("YYYY-MM-DD-X");
+  expect(docxName("     100 Market St", "A1", currentMoment)).toEqual("100-Market-St-A1-" + currentMomentFormatted);
   expect(docxName("100 Market St", "A1", currentMoment)).toEqual("100-Market-St-A1-" + currentMomentFormatted);
   expect(docxName("100MarketSt", "A1", currentMoment)).toEqual("100MarketSt-A1-" + currentMomentFormatted);
 });
