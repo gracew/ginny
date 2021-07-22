928b18be3b79a56864f31edf4b605e45fe6169a5
Jason Zhong <51380978+Jzhong456@users.noreply.github.com>
2021-07-22 14:33:02 -0400

    Fixed test and added case for trimming  (#11)
    * fix test based on document name updates
    
    * add a test on the trimming of the file name
    
    Co-authored-by: Grace Wang <gracewang92@gmail.com>

--- test/generate.test.ts
+++ test/generate.test.ts
@@ -1,5 +1,10 @@
+import moment from "moment";
 import { docxName } from "../pages/api/generate";
 
 it("generated doc name", () => {
-  expect(docxName("100 Market St", "A1", "2021-07-20")).toEqual("100-Market-St-A1-2021-07-20");
+  const currentMoment = moment();
+  const currentMomentFormatted = currentMoment.format("YYYY-MM-DD-X");
+  expect(docxName("     100 Market St", "A1", currentMoment)).toEqual("100-Market-St-A1-" + currentMomentFormatted);
+  expect(docxName("100 Market St", "A1", currentMoment)).toEqual("100-Market-St-A1-" + currentMomentFormatted);
+  expect(docxName("100MarketSt", "A1", currentMoment)).toEqual("100MarketSt-A1-" + currentMomentFormatted);
 });
