4cd9ddbdecd536033e4e5c796cb2fd9cbbc9693d
Jason <jrzhong2009@gmail.com>
2021-07-26 18:56:28 -0400

    added a test

--- test/generate.test.ts
+++ test/generate.test.ts
@@ -6,9 +6,10 @@ it("generated doc name", () => {
   const currentMoment = moment();
   const currentMomentFormatted = currentMoment.format("YYYY-MM-DD-X");
   expect(docxName("     100 Market St", "A1", currentMoment)).toEqual("100-Market-St-A1-" + currentMomentFormatted);
+  expect(docxName("     100 Market St      ", "    A1     ", currentMoment)).toEqual("100-Market-St-A1-" + currentMomentFormatted);
   expect(docxName("100 Market St", "A1", currentMoment)).toEqual("100-Market-St-A1-" + currentMomentFormatted);
   expect(docxName("100MarketSt", "A1", currentMoment)).toEqual("100MarketSt-A1-" + currentMomentFormatted);
   expect(addNewLine("Hello\nworld\n")).toEqual("Hello<w:br/>world<w:br/>")
   expect(addNewLine("Hello\nworld\nfoo\nbar")).toEqual("Hello<w:br/>world<w:br/>foo<w:br/>bar")
-  
+
 });
