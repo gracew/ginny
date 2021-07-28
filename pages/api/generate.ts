a0dac100d80b35312771855945b9dae4f0ca39ba
Jason Zhong <51380978+Jzhong456@users.noreply.github.com>
2021-07-28 14:35:01 -0400

    Gw/generate tests (#15)
    * refactor total calculations into a separate testable function
    
    * Add function that calculates total amount needed
    
    * added a test for the property generation
    
    * added a test for the property generation
    
    * added a test for the property generation
    
    * split test between zeroed values and non-zeroed value
    
    * modified the code to be cleaner and corrected the loop
    
    * changed an error and tested the prorated values
    
    Co-authored-by: Grace Wang <gracewang92@gmail.com>

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -8,7 +8,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import os from "os";
 import path from "path";
 import replace from "string-replace-stream";
-import { v4 as uuidv4 } from "uuid";
 import { client } from "./client";
 
 function formatAmount(x: number) {
@@ -22,13 +21,63 @@ type Data = {
 const gcs = new Storage();
 const templateFilename = "reservation_agreement_template_2021-07-14.docx";
 
-export function docxName(propertyName:string, unitNumber:string, currentMoment: moment.Moment): string{
+export function docxName(propertyName: string, unitNumber: string, currentMoment: moment.Moment): string {
   const name = propertyName.trim() + "-" + unitNumber.trim() + "-" + currentMoment.format("YYYY-MM-DD-X")
   return name.replace(/ /g, "-")
 }
 
-export function createLineBreak(customText:string){
-  return customText.replace(/\n/g,"<w:br/>")
+export function createLineBreak(customText: string) {
+  return customText.replace(/\n/g, "<w:br/>")
+}
+
+interface Amounts {
+  APPLICATION_AMOUNT_DUE: number;
+  MOVEIN_AMOUNT_DUE: number;
+  PRORATED_RENT: number;
+  PRORATED_PARKING: number;
+  PRORATED_STORAGE: number;
+  PRORATED_TRASH: number;
+  PRORATED_PET_RENT: number;
+}
+interface Totals {
+  FIRST_MONTH_DATES: string;
+  amounts: Amounts;
+}
+
+export function computeTotals(data: any): Totals {
+  const { property, ...otherInputs } = data;
+  const {
+    aptNo, leaseTermMonths, moveInDate, monthlyRent, parking, storage, petRent, petFee, concessions
+  } = otherInputs;
+  const moveInDateMoment = moment(moveInDate);
+  const lastDayMonth = moveInDateMoment.clone().endOf("month");
+  const prorateAmount = (lastDayMonth.diff(moveInDateMoment, "days") + 1) / lastDayMonth.daysInMonth();
+  const proratedRent = prorateAmount * monthlyRent;
+
+  const applicationAmountDue = property.application_fee || property.reservation_fee
+    ? (property.application_fee || 0) + (property.reservation_fee || 0)
+    : undefined;
+
+  const proratedParking = prorateAmount * parking
+  const proratedTrash = prorateAmount * property.trash_fee
+  const proratedStorage = prorateAmount * storage
+  const proratedPetRent = prorateAmount * petRent
+  let moveInAmountDue = proratedRent
+  moveInAmountDue += (petFee || 0) + (property.admin_fee || 0) + proratedTrash + proratedParking + proratedStorage + proratedPetRent;
+  const firstMonthDates = `${moveInDateMoment.format("MM/DD/YYYY")} - ${lastDayMonth.format("MM/DD/YYYY")}`;
+
+  return {
+    FIRST_MONTH_DATES: firstMonthDates,
+    amounts: {
+    APPLICATION_AMOUNT_DUE: applicationAmountDue,
+    MOVEIN_AMOUNT_DUE: moveInAmountDue,
+    PRORATED_RENT: proratedRent,
+    PRORATED_PARKING: proratedParking,
+    PRORATED_STORAGE: proratedStorage,
+    PRORATED_TRASH: proratedTrash,
+    PRORATED_PET_RENT: proratedPetRent,
+    }
+  }
 }
 
 async function handler(
@@ -50,15 +99,8 @@ async function handler(
   const {
     aptNo, leaseTermMonths, moveInDate, monthlyRent, parking, storage, petRent, petFee, concessions
   } = otherInputs;
+  const totals = computeTotals(req.body);
 
-  const moveInDateMoment = moment(moveInDate);
-  const lastDayMonth = moveInDateMoment.clone().endOf("month");
-  const prorateAmount = (lastDayMonth.diff(moveInDateMoment, "days") + 1) / lastDayMonth.daysInMonth();
-  const proratedRent = prorateAmount * monthlyRent;
-
-  const applicationAmountDue = property.application_fee || property.reservation_fee
-    ? (property.application_fee || 0) + (property.reservation_fee || 0)
-    : undefined;
   const fullAddress = `${property.address}, ${property.city}, ${property.state}  ${property.zip}`;
 
   let newStream = xml.nodeStream()
@@ -68,37 +110,19 @@ async function handler(
     .pipe(replace("LEASE_TERM", `${leaseTermMonths} months`))
     .pipe(replace("APPLICATION_FEE", property.application_fee ? formatAmount(property.application_fee) : "N/A"))
     .pipe(replace("RESERVATION_FEE", property.reservation_fee ? formatAmount(property.reservation_fee) : "N/A"))
-    .pipe(replace("APPLICATION_AMOUNT_DUE", applicationAmountDue ? formatAmount(applicationAmountDue) : "N/A"))
-    .pipe(replace("FIRST_MONTH_DATES", `${moveInDateMoment.format("MM/DD/YYYY")} - ${lastDayMonth.format("MM/DD/YYYY")}`))
-    .pipe(replace("PRORATED_RENT", formatAmount(proratedRent)));
+    .pipe(replace("PET_FEE", petFee ? formatAmount(petFee) : "N/A"))
+    .pipe(replace("ADMIN_FEE", property.admin_fee ? formatAmount(property.admin_fee) : "N/A"))
+    .pipe(replace("CUSTOM_TEXT", createLineBreak(property.custom_text) || ""))
+    .pipe(replace("CONCESSIONS", concessions || ""))
+    .pipe(replace("FIRST_MONTH_DATES", totals.FIRST_MONTH_DATES));
 
-  const optionalRents = {
-    PRORATED_PARKING: parking,
-    PRORATED_STORAGE: storage,
-    PRORATED_TRASH: property.trash_fee,
-    PRORATED_PET_RENT: petRent,
-  }
-
-  let moveInAmountDue: number = proratedRent;
-  Object.entries(optionalRents).forEach(([key, monthlyRent]) => {
+  Object.entries(totals.amounts).forEach(([key, monthlyRent]) => {
     if (monthlyRent) {
-      const proratedRent = prorateAmount * monthlyRent;
-      moveInAmountDue += proratedRent;
-      newStream = newStream.pipe(replace(key, formatAmount(proratedRent)));
+      newStream = newStream.pipe(replace(key, formatAmount(monthlyRent)));
     } else {
       newStream = newStream.pipe(replace(key, "N/A"));
     }
   });
-
-  moveInAmountDue += petFee || 0;
-  newStream = newStream.pipe(replace("PET_FEE", petFee ? formatAmount(petFee) : "N/A"));
-  moveInAmountDue += property.admin_fee || 0;
-  newStream = newStream.pipe(replace("ADMIN_FEE", property.admin_fee ? formatAmount(property.admin_fee) : "N/A"));
-
-  newStream = newStream
-    .pipe(replace("MOVEIN_AMOUNT_DUE", formatAmount(moveInAmountDue)))
-    .pipe(replace("CUSTOM_TEXT", createLineBreak(property.custom_text) || ""))
-    .pipe(replace("CONCESSIONS", concessions || ""));
 
   zip.file("word/document.xml", newStream);
   const outFileName = docxName(property.address, aptNo, moment()) + ".docx";
--- test/generate.test.ts
+++ test/generate.test.ts
@@ -1,5 +1,5 @@
 import moment from "moment";
-import { docxName } from "../pages/api/generate";
+import { computeTotals, docxName } from "../pages/api/generate";
 import { createLineBreak } from "../pages/api/generate";
 
 it("generated doc name", () => {
@@ -17,4 +17,68 @@ it("new lines are converted to line breaks", () => {
   expect(createLineBreak("Hello\nworld\nfoo\nbar")).toEqual("Hello<w:br/>world<w:br/>foo<w:br/>bar")
   expect(createLineBreak("")).toEqual("")
   expect(createLineBreak("\t")).toEqual("\t")
+});
+
+it("Calculating total amount",() =>{
+  const property = {
+    address: "Address",
+    application_fee: 100,
+    reservation_fee: 100,
+    admin_fee: 100,
+    trash_fee: 10,
+    custom_text: "Hello"
+  }
+
+  const d = new Date(2021,7,27);
+
+  const data = {
+    property: property,
+    aptNo: "A1", 
+    leaseTermMonths: 12, 
+    moveInDate: d, 
+    monthlyRent: 200, 
+    parking: 20, 
+    storage: 15, 
+    petRent: 10, 
+    petFee: 25, 
+    concessions: ""
+  }
+  const totals = computeTotals(data)
+  expect(totals.amounts.APPLICATION_AMOUNT_DUE).toEqual(200)
+  expect(totals.amounts.PRORATED_RENT.toFixed(2)).toEqual("32.26")
+  expect(totals.amounts.PRORATED_PARKING.toFixed(2)).toEqual("3.23")
+  expect(totals.amounts.PRORATED_PET_RENT.toFixed(2)).toEqual("1.61")
+  expect(totals.amounts.PRORATED_TRASH.toFixed(2)).toEqual("1.61")
+  expect(totals.amounts.PRORATED_STORAGE.toFixed(2)).toEqual("2.42")
+  expect(totals.amounts.MOVEIN_AMOUNT_DUE.toFixed(2)).toEqual("166.13")
+});
+
+it("Calculate total when extra fees are zero",() =>{
+  const property = {
+    address: "Address",
+    application_fee: 100,
+    reservation_fee: 100,
+    admin_fee: 50,
+    trash_fee: 0,
+    custom_text: "Hello"
+  }
+
+  const d = new Date(2021,7,27);
+
+  const data = {
+    property: property,
+    aptNo: "A1", 
+    leaseTermMonths: 12, 
+    moveInDate: d, 
+    monthlyRent: 200, 
+    parking: 0, 
+    storage: 0, 
+    petRent: 0, 
+    petFee: 0, 
+    concessions: ""
+  }
+  const totals = computeTotals(data)
+  expect(totals.amounts.APPLICATION_AMOUNT_DUE).toEqual(200)
+  expect(totals.amounts.PRORATED_RENT.toFixed(2)).toEqual("32.26")
+  expect(totals.amounts.MOVEIN_AMOUNT_DUE.toFixed(2)).toEqual("82.26")
 });
